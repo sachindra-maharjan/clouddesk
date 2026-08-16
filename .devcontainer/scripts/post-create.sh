8d35aca0993a7b239927bf212ea65d9b936209ff
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2026-08-16 15:31:21 -0400

    feat: add devcontainer configuration with Dockerfile, post-start/create scripts, and extension requirements

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+echo "Executing post-create.sh script"
+
+
+echo "Exiting post-create.sh script"
+exit 0
---
+++ .devcontainer/scripts/post-start.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+echo "Executing post-start.sh script"
+
+
+echo "Exiting post-start.sh script"
+exit 0
